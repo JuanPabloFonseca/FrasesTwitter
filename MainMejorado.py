d8ab1aa2e577d040588fd7e73b942077a67e9fd4
JPFC1991 <jpfonsec@hotmail.com>
2016-06-30 13:14:55 -0500

    Cuento la dimensión de la matriz tfidf

--- MainMejorado.py
+++ MainMejorado.py
@@ -1,6 +1,7 @@
 import LDA_cluster
 import ObtenerTweets
 import LimpiarTweets
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 import numpy
 import pandas as pd
@@ -13,24 +14,40 @@ def principal():
     #s=pd.Series(s)
     #print(s.str.get_dummies(sep=','))
 
-    topicos=obtenerTopicosTweets() #obtiene tópicos
+    [topicos,listaTweets]=obtenerTopicosYTweets() #obtiene tópicos
     topicos=pd.Series(topicos)
     tablaTopicos=topicos.str.get_dummies(sep=',')#genera la matriz binaria
     print(tablaTopicos)
+    tweetsLimpios=LimpiarTweets.limpiarTexto(listaTweets)
+    matriz=obtenerTfIdf(tweetsLimpios)
+
     #print(tablaTopicos.std())
     #print(tablaTopicos["econom?a"][3])
 
 
 
-def obtenerTopicosTweets():
-    data = []
+def obtenerTopicosYTweets():
+    clasificacion = []
+    tweets = []
     with open('tass/tass_2015/tweetsTopic.txt') as f:
         for line in f:
-            data.append(line.split('\\',1)[0])
+            contenido=line.split('\\\\\\', 1)
+            clasificacion.append(contenido[0])
+            tweets.append(contenido[1])
 
-    return data
+    return [clasificacion,tweets]
 
 
+def obtenerTfIdf(datos):
+    docs = []
+    for t in datos:
+        docs.append(', '.join(str(x) for x in t))
+    # Use tf-idf features
+    tfidf_vectorizer = TfidfVectorizer(min_df=2, lowercase=False, encoding='utf8mb4')
+
+    # Calcula idf mediante log base 2 de (1 + N/n_i), donde N es el total de tweets, y n_i es el numero de documentos donde aparece la palabra
+    tfidf = tfidf_vectorizer.fit_transform(docs)
+    print(tfidf.shape)
 
 if __name__ == '__main__':
     principal()
