fdbc67ba27e65aa5dfbb45ad3082474955f35c97
JPFC1991 <jpfonsec@hotmail.com>
2016-07-04 09:18:21 -0500

    Aplico PCA y luego regresión logística.

--- MainMejorado.py
+++ MainMejorado.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3.5
-
 import LDA_cluster
 import ObtenerTweets
 import LimpiarTweets
@@ -9,9 +7,10 @@ import ML_MejoraAlBaseline as ml
 
 import numpy
 import pandas as pd
-# import statsmodels
+import statsmodels.api as sm
 import pylab
 
+from sklearn.decomposition import PCA
 from sklearn.cross_validation import train_test_split
 
 tfidf_vectorizer = TfidfVectorizer(min_df=2, lowercase=False, encoding='utf8mb4')
@@ -26,7 +25,10 @@ def principal():
     # X_train, X_test, y_train, y_test = train_test_split(listaTweets, topicos, test_size=0.25, random_state=42)
 
     tweetsLimpios=LimpiarTweets.limpiarTexto(listaTweets)
+
+
     X = obtenerTfIdf(tweetsLimpios)
+
 
     # X = StandardScaler().fit_transform(X)
 
@@ -34,7 +36,56 @@ def principal():
     tweetsLimpiosTest = LimpiarTweets.limpiarTexto(listaTweetsTest)
     X_te = calcularTFIDF(tweetsLimpiosTest)
 
-    ml.clasificadores_supervisados(X, topicosTrain, X_te, topicosTest)
+    #ml.clasificadores_supervisados(X, topicosTrain, X_te, topicosTest)
+
+
+    #REGRESIÓN LOGÍSTICA. Para eso se junta la matriz binaria de tópicos (pandas.core.frame.DataFrame)
+    #                                     y la matriz de los scores tfidf (scipy.sparse.csr.csr_matrix)
+
+    Xarr=X.toarray()                  #pasas a ndarray
+    pca=PCA(n_components=0.8)         #PCA al 80% (determinar porcentaje según logit lo requiera)
+    Xpca=pca.fit_transform(Xarr)      #reduces dimensionalidad
+    Xdf = pd.DataFrame(Xpca)          #pasas a DataFrame
+    print("Ya se hizo pca")
+    print(Xdf.shape)
+
+    RLdf= topicosTrain
+    RLdf=pd.concat([RLdf,Xdf],axis=1) #juntas matriz binaria de tópicos y Xdf (tfidf con PCA)
+    print(RLdf.shape)
+    print(RLdf.head())
+
+
+    train_cols = RLdf.columns[10:]
+    #revisar si son o no demasiadas columnas para la regresión logística
+
+    print("ahora se va a hacer logit")
+    logit = sm.Logit(RLdf['cine'], RLdf[train_cols])
+    RLcine = logit.fit()
+    print(RLcine.summary())
+    '''logit = sm.Logit(RLdf['deportes'], RLdf[train_cols])
+    RLdeportes = logit.fit()
+    logit = sm.Logit(RLdf['econom?a'], RLdf[train_cols])
+    RLeco = logit.fit()
+    logit = sm.Logit(RLdf['entretenimiento'], RLdf[train_cols])
+    RLentret = logit.fit()
+    logit = sm.Logit(RLdf['f?tbol'], RLdf[train_cols])
+    RLfutbol = logit.fit()
+    logit = sm.Logit(RLdf['literatura'], RLdf[train_cols])
+    RLlite = logit.fit()
+    logit = sm.Logit(RLdf['m?sica'], RLdf[train_cols])
+    RLmusica = logit.fit()
+    logit = sm.Logit(RLdf['otros'], RLdf[train_cols])
+    RLotros = logit.fit()
+    logit = sm.Logit(RLdf['pol?tica'], RLdf[train_cols])
+    RLpolitica = logit.fit()
+    logit = sm.Logit(RLdf['tecnolog?a'], RLdf[train_cols])
+    RLtecno = logit.fit()'''
+
+    X_te_arr=X_te.toarray()
+    X_te_pca=pca.transform(X_te_arr)
+    X_te_df=pd.DataFrame(X_te_pca)
+
+    print(RLcine.predict(X_te_df[:])) #debería imprimir 1s y 0s según la predicción
 
     # X = word2vec
     # ml.clasificadores_supervisados(X, topicos)
