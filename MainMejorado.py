502d03736f1e3fd1414a44078b11c848af2bc7a4
JPFC1991 <jpfonsec@hotmail.com>
2016-07-07 12:40:24 -0500

    Modelos de RL creados (y optimizados) con Train sin ver a Test, y luego probados con Test.

--- MainMejorado.py
+++ MainMejorado.py
@@ -78,9 +78,16 @@ def principal():
     # X_te_df=pd.DataFrame(X_te_pca)
 
     print("Se va a hacer Regresión Logística")
-    PredDataFrame=regresionLogistica(RLdf,X_te_df,topicosTest)
-    #print(PredDataFrame)
 
+    #primer intento:
+    #PredDataFrame=regresionLogistica(RLdf,X_te_df,topicosTest)
+
+    #primer intento optimizado:
+    #PDF=regLogOptimizada1(tweetsLimpios,topicosTrain,tweetsLimpiosTest,topicosTest)
+
+    #segundo intento optimizado:
+    PredDF=regLogOptimizada2(RLdf,X_te_df,topicosTest)
+    print(PredDF)
 
     # X = word2vec
     # ml.clasificadores_supervisados(X, topicos)
@@ -89,35 +96,95 @@ def regresionLogistica(RLdf,X_te_df,topicosTest):
 
     model = OneVsRestClassifier(linear_model.LogisticRegression())
     model.fit(X=RLdf[RLdf.columns[10:]], y=RLdf[RLdf.columns[:10]])
+    PredTrain = pd.DataFrame(model.predict(RLdf[RLdf.columns[10:]]))
+
+    print("\nDel train: ")
+    for i in range(0, 10):
+        prt, rct, fbt, sut = precision_recall_fscore_support(RLdf[RLdf.columns[:10]].iloc[:, i], PredTrain[i], average='binary')
+        print("{0}: Precision {1}, Recall {2}, F1 {3}".format(RLdf.columns[i], prt, rct, fbt))
+
+
+
     PredDF = pd.DataFrame(model.predict(X_te_df))
 
-    #de manera compacta:
-    '''pr0, rc0, fb0, su0 = precision_recall_fscore_support(topicosTest['cine'], PredDF[0], average='binary')
+    print("\nDel test: ")
+    pr0, rc0, fb0, su0 = precision_recall_fscore_support(topicosTest['cine'], PredDF[0], average='binary')
     print("Cine: Precision {0}, Recall {1}, F1 {2}".format(pr0, rc0, fb0))
     for i in range(1,9):
         pr, rc, fb, su = precision_recall_fscore_support(topicosTest.iloc[:,i], PredDF[i+1], average='binary')
-        print("Precision {0}, Recall {1}, F1 {2}".format(pr, rc, fb))'''
-
-    pr0, rc0, fb0, su0 = precision_recall_fscore_support(topicosTest['cine'], PredDF[0], average='binary')
-    print("Cine: Precision {0}, Recall {1}, F1 {2}".format(pr0, rc0, fb0))
-    pr2, rc2, fb2, su2 = precision_recall_fscore_support(topicosTest['economía'], PredDF[2], average='binary')
-    print("Economía: Precision {0}, Recall {1}, F1 {2}".format(pr2, rc2, fb2))
-    pr3, rc3, fb3, su3 = precision_recall_fscore_support(topicosTest.iloc[:,2], PredDF[3], average='binary')
-    print("Entretenimiento: Precision {0}, Recall {1}, F1 {2}".format(pr3, rc3, fb3))
-    pr4, rc4, fb4, su4 = precision_recall_fscore_support(topicosTest['fútbol'], PredDF[4], average='binary')
-    print("Fútbol: Precision {0}, Recall {1}, F1 {2}".format(pr4, rc4, fb4))
-    pr5, rc5, fb5, su5 = precision_recall_fscore_support(topicosTest['literatura'], PredDF[5], average='binary')
-    print("Literatura: Precision {0}, Recall {1}, F1 {2}".format(pr5, rc5, fb5))
-    pr6, rc6, fb6, su6 = precision_recall_fscore_support(topicosTest['música'], PredDF[6], average='binary')
-    print("Música: Precision {0}, Recall {1}, F1 {2}".format(pr6, rc6, fb6))
-    pr7, rc7, fb7, su7 = precision_recall_fscore_support(topicosTest.iloc[:,6], PredDF[7], average='binary')
-    print("Otros: Precision {0}, Recall {1}, F1 {2}".format(pr7, rc7, fb7))
-    pr8, rc8, fb8, su8 = precision_recall_fscore_support(topicosTest.iloc[:,7], PredDF[8], average='binary')
-    print("Política: Precision {0}, Recall {1}, F1 {2}".format(pr8, rc8, fb8))
-    pr9, rc9, fb9, su9 = precision_recall_fscore_support(topicosTest.iloc[:,8], PredDF[9], average='binary')
-    print("Tecnología: Precision {0}, Recall {1}, F1 {2}".format(pr9, rc9, fb9))
+        print("{0}: Precision {1}, Recall {2}, F1 {3}".format(topicosTest.columns[i],pr, rc, fb))
 
     return PredDF
+
+def regLogOptimizada1(tTrain,topicTrain,tTest,topicTest):#primer intento
+
+    #esto es increiblemente ineficiente y pierde el punto????? creo que sí
+    mins=[3,4,5,3,4,4,4,2,1,4]#se vio que estos valores para min_df optimizaban el F1 de cada topico en train
+    PDF=pd.DataFrame()
+    for i in range(10):
+        tfidf_vectorizer = TfidfVectorizer(max_features=4000, min_df=mins[i], lowercase=False, encoding='utf8mb4', ngram_range=(2, 5))
+        X_train=pd.DataFrame(obtenerTfIdfParticular(tTrain,tfidf_vectorizer).toarray())
+        logReg=linear_model.LogisticRegression()
+        logReg.fit(X=X_train,y=topicTrain[topicTrain.columns[i]])
+        PredTrain=pd.DataFrame(logReg.predict(X_train))
+        PDF=pd.concat([PDF,PredTrain],axis=1)
+        prt, rct, fbt, sut = precision_recall_fscore_support(topicTrain[topicTrain.columns[i]], PredTrain, average='binary')
+        print("{0}: Precision {1}, Recall {2}, F1 {3}".format(topicTrain.columns[i], prt, rct, fbt))
+
+
+    #falta evaluar en test con esos modelos... pero no lo voy a hacer porque esto es increiblemente ineficiente
+
+    return PDF
+
+
+def regLogOptimizada2(RLdf,X_te_df,topicosTest):
+    cs=[0.86, 0.01, 0.97, 0.99, 0.84, 0.01, 0.99, 1.0, 0.92, 0.01]
+
+    #este código comentado fue para encontrar las mejores cs para cada tópico, y son las que están aquí arriba
+    '''for i in range(10):
+        fbmax=0
+        jmax=0.01
+        for j in range (1,101,1):
+            logReg=linear_model.LogisticRegression(C=j/100)
+            logReg.fit(X=RLdf[RLdf.columns[10:]], y=RLdf[RLdf.columns[i]])
+            PredTrain=pd.DataFrame(logReg.predict(X=RLdf[RLdf.columns[10:]]))
+            prt, rct, fbt, sut = precision_recall_fscore_support(RLdf[RLdf.columns[i]], PredTrain, average='binary')
+            if fbt > fbmax:
+                fbmax=fbt
+                jmax=j/100
+        cs[i]=jmax
+    print(cs)'''
+
+    #se obtienen las mejores regresiones logísticas para el conjunto de entrenamiento (SIN ver al conjunto de prueba)
+    print("\nDel train: ")
+    PDF = pd.DataFrame()
+    logReg=[]
+    for i in range(10):
+        logReg.append(linear_model.LogisticRegression(C=cs[i]))
+        logReg[i].fit(X=RLdf[RLdf.columns[10:]], y=RLdf[RLdf.columns[i]])
+        PredTrain = pd.DataFrame(logReg[i].predict(X=RLdf[RLdf.columns[10:]]))
+        PDF = pd.concat([PDF, PredTrain], axis=1)
+        prt, rct, fbt, sut = precision_recall_fscore_support(RLdf[RLdf.columns[i]], PredTrain, average='binary')
+        print("{0}: Precision {1}, Recall {2}, F1 {3}".format(RLdf.columns[i], prt, rct, fbt))
+
+
+    #luego, se ajustan los datos de prueba a las regresiones:
+    print("\nDel test: ")
+    PredDF = pd.DataFrame()
+    PredTest = pd.DataFrame(logReg[0].predict(X_te_df))
+    PredDF = pd.concat([PredDF, PredTest], axis=1)
+    pr0, rc0, fb0, su0 = precision_recall_fscore_support(topicosTest['cine'], PredDF, average='binary')
+    print("Cine: Precision {0}, Recall {1}, F1 {2}".format(pr0, rc0, fb0))
+    PredDF = pd.concat([PredDF, pd.DataFrame(np.zeros((PredDF.shape[0],1),dtype=np.int))], axis=1)
+    for i in range(1, 9):
+        PredTest = pd.DataFrame(logReg[i+1].predict(X_te_df))
+        PredDF = pd.concat([PredDF, PredTest], axis=1)
+        pr, rc, fb, su = precision_recall_fscore_support(topicosTest.iloc[:, i], PredDF.iloc[:,i + 1], average='binary')
+        print("{0}: Precision {1}, Recall {2}, F1 {3}".format(topicosTest.columns[i], pr, rc, fb))
+
+
+    return PredDF
+
 
 
 def obtenerTopicosYTweets(str):
@@ -147,6 +214,18 @@ def obtenerTfIdf(datos):
     # Calcula idf mediante log base 2 de (1 + N/n_i), donde N es el total de tweets, y n_i es el numero de documentos donde aparece la palabra
     tfidf = tfidf_vectorizer.fit_transform(docs)
     return tfidf
+
+
+def obtenerTfIdfParticular(datos,vect):
+    docs = []
+    for t in datos:
+        docs.append(', '.join(str(x) for x in t))
+    # Use tf-idf features
+
+    # Calcula idf mediante log base 2 de (1 + N/n_i), donde N es el total de tweets, y n_i es el numero de documentos donde aparece la palabra
+    tfidf = vect.fit_transform(docs)
+    return tfidf
+
 
 def calcularTFIDF(datos):
     docs = []
